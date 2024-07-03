64ab131aae0fa1da945852c31e860c0e490d454b
ilyes arous <ilyesarous9645@gamil.com>
2024-07-03 12:54:35 +0100

    create new offers + show all offers are available+ you can search for a specefic offer

--- authentification-next/src/components/dashboard/ShowOffers.tsx
+++ authentification-next/src/components/dashboard/ShowOffers.tsx
@@ -35,7 +35,7 @@ const ShowOffers = ({ position }: any) => {
       </span>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full">
         {offers
-          .filter((offer) =>
+          .filter((offer: Offer) =>
             search !== "" ? offer.positioname.includes(search) : offer
           )
           .map((offer: Offer) => {
